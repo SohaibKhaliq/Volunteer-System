0da3f89b2789cf41a5acd99dadbf0fea203bc64b
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-23 22:57:20 +0500

    feat: Create migration for resources table with necessary fields

---
+++ apps/api/database/migrations/1720000001000_resources.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'resources'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table.integer('quantity').defaultTo(0)
+      table.string('status').defaultTo('Available')
+      table.integer('organization_id').unsigned().references('id').inTable('organizations').onDelete('CASCADE')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
