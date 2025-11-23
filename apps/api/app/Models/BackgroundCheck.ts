57241ca8c05279b5c65961d62bd146ef862cdb13
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-24 03:08:01 +0500

    feat: add BackgroundCheck model for managing background check data

---
+++ apps/api/app/Models/BackgroundCheck.ts
@@ -0,0 +1,35 @@
+import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
+import User from './User'
+
+export default class BackgroundCheck extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column({ columnName: 'user_id' })
+  public userId: number
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+  @column()
+  public status?: string
+
+  @column()
+  public result?: string
+
+  @column.dateTime({ columnName: 'requested_at' })
+  public requestedAt?: DateTime
+
+  @column.dateTime({ columnName: 'completed_at' })
+  public completedAt?: DateTime
+
+  @column()
+  public notes?: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
