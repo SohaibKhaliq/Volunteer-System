4b3309b1c8bbe6e03c2a0950b61fb8bd0ca9d13c
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-23 22:51:59 +0500

    feat: Add Survey model with fields for id, title, description, status, and responses association

---
+++ apps/api/app/Models/Survey.ts
@@ -0,0 +1,26 @@
+import { BaseModel, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
+import SurveyResponse from './SurveyResponse'
+
+export default class Survey extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public description?: string
+
+  @column()
+  public status: string
+
+  @hasMany(() => SurveyResponse)
+  public responses: HasMany<typeof SurveyResponse>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
