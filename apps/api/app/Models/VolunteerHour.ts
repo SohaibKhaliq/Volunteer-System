7cbb2935724637e8db4f03693f18433dc9bfcf57
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-23 22:58:37 +0500

    feat: Add VolunteerHour model for tracking volunteer hours and associated events

---
+++ apps/api/app/Models/VolunteerHour.ts
@@ -0,0 +1,36 @@
+import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
+import User from './User'
+import Event from './Event'
+
+export default class VolunteerHour extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public userId: number
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+  @column()
+  public eventId: number
+
+  @belongsTo(() => Event)
+  public event: BelongsTo<typeof Event>
+
+  @column.date()
+  public date: DateTime
+
+  @column()
+  public hours: number
+
+  @column()
+  public status: string // 'Pending', 'Approved', 'Rejected'
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
