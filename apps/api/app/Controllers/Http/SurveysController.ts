5bf13b79077956a822fe498363525d1c860832f9
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-23 22:53:44 +0500

    feat: Implement SurveysController with CRUD methods for managing surveys

---
+++ apps/api/app/Controllers/Http/SurveysController.ts
@@ -0,0 +1,30 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Survey from 'App/Models/Survey'
+
+export default class SurveysController {
+  public async index({ request }: HttpContextContract) {
+    return Survey.query().preload('responses')
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['title', 'description', 'status'])
+    return Survey.create(data)
+  }
+
+  public async show({ params }: HttpContextContract) {
+    return Survey.query().where('id', params.id).preload('responses').firstOrFail()
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    const survey = await Survey.findOrFail(params.id)
+    const data = request.only(['title', 'description', 'status'])
+    survey.merge(data)
+    await survey.save()
+    return survey
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const survey = await Survey.findOrFail(params.id)
+    await survey.delete()
+  }
+}
