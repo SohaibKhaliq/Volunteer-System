fe22e0a5492b1c9deb4f8c382b5d1d9982de1654
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-23 23:03:42 +0500

    feat: Add migration for survey_responses table with relationships to surveys and users

---
+++ apps/api/database/migrations/1720000004000_survey_responses.ts
@@ -0,0 +1,19 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'survey_responses'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('survey_id').unsigned().references('id').inTable('surveys').onDelete('CASCADE')
+      table.integer('user_id').unsigned().nullable().references('id').inTable('users').onDelete('SET NULL')
+      table.json('answers').notNullable()
+      table.timestamp('created_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
