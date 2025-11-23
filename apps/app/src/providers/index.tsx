3a96d0837846fcbc6a770a89d7cde83280c2095d
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-24 00:09:20 +0500

    refactor: Update Providers component to remove AppProvider and enhance query client options

--- apps/app/src/providers/index.tsx
+++ apps/app/src/providers/index.tsx
@@ -2,13 +2,15 @@ import i18n from '@/lib/i18n';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
-import AppProvider from './app-provider';
+import { Toaster } from 'sonner';
 import ThemeProvider from './theme-provider';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      suspense: true
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 5 * 60 * 1000
     }
   }
 });
@@ -19,7 +21,8 @@ const Providers = ({ children }: { children: ReactNode }) => {
       <QueryClientProvider client={queryClient}>
         <I18nextProvider i18n={i18n} defaultNS={'common'}>
           <ThemeProvider defaultTheme="light" storageKey="theme">
-            <AppProvider>{children}</AppProvider>
+            {children}
+            <Toaster position="top-right" richColors />
           </ThemeProvider>
         </I18nextProvider>
       </QueryClientProvider>
