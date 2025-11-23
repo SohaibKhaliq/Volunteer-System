4e31af25d2ab6edd567de3650b576d358758c05a
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-23 22:56:43 +0500

    fix: Remove unused request parameter from index method in TasksController

--- apps/api/app/Controllers/Http/TasksController.ts
+++ apps/api/app/Controllers/Http/TasksController.ts
@@ -2,7 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Task from 'App/Models/Task'
 
 export default class TasksController {
-  public async index({ request, response }: HttpContextContract) {
+  public async index({ response }: HttpContextContract) {
     const tasks = await Task.query().orderBy('start_at', 'asc')
     return response.ok(tasks)
   }
