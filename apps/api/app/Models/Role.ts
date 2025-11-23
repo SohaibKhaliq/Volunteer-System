c23a084503ad5f99654bd58b5c63c8de9042808b
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-23 11:55:35 +0500

    add Role model: implement structure for roles with relevant fields and relationships

---
+++ apps/api/app/Models/Role.ts
@@ -0,0 +1,29 @@
+import { BaseModel, column, manyToMany } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
+import User from './User'
+
+export default class Role extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public description?: string
+
+  @manyToMany(() => User, {
+    pivotTable: 'user_roles',
+    localKey: 'id',
+    pivotForeignKey: 'role_id',
+    relatedKey: 'id',
+    pivotRelatedForeignKey: 'user_id'
+  })
+  public users: any
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
