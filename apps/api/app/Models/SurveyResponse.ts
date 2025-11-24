6b3231992db2d168c757359e14e4815ec824d243
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-24 12:20:09 +0500

    feat: refactor SurveyResponse model to improve structure and clarity

--- apps/api/app/Models/SurveyResponse.ts
+++ apps/api/app/Models/SurveyResponse.ts
@@ -1,34 +1,4 @@
 import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
-import Survey from './Survey'
-import { DateTime } from 'luxon'
-
-export default class SurveyResponse extends BaseModel {
-  @column({ isPrimary: true })
-  public id: number
-
-  @column()
-  @column({ columnName: 'survey_id' })
-  public surveyId: number
-
-  @belongsTo(() => Survey)
-  public survey: BelongsTo<typeof Survey>
-
-  @column()
-  public userId?: number
-
-  @column()
-  public answers?: string // JSON
-
-  @column()
-  public ipAddress?: string
-
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
-
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
-}
-import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
 import { DateTime } from 'luxon'
 import Survey from 'App/Models/Survey'
 import User from 'App/Models/User'
@@ -37,21 +7,27 @@ export default class SurveyResponse extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
-  @column()
+  @column({ columnName: 'survey_id' })
   public surveyId: number
 
   @belongsTo(() => Survey)
   public survey: BelongsTo<typeof Survey>
 
-  @column()
+  @column({ columnName: 'user_id' })
   public userId?: number
 
   @belongsTo(() => User)
   public user: BelongsTo<typeof User>
 
   @column()
-  public answers: string // JSON string
+  public answers?: string // JSON string
+
+  @column({ columnName: 'ip_address' })
+  public ipAddress?: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
 }
