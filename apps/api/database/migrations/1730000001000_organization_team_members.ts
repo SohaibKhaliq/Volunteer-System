6a8608a8e6e7968b9c01c8f53e997b2a7f2ff5f5
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-24 08:56:34 +0500

    feat: create migration for organization_team_members table with relevant fields and relationships

---
+++ apps/api/database/migrations/1730000001000_organization_team_members.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'organization_team_members'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('organization_id').unsigned().references('id').inTable('organizations').onDelete('CASCADE')
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.string('role').defaultTo('Member')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
