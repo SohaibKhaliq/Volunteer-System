bbf1e34d04daa0e9b85de0ee24e8117e52278bf3
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-26 09:42:16 +0500

    feat: enhance state management by adding user-related methods and properties

--- apps/app/src/lib/store.ts
+++ apps/app/src/lib/store.ts
@@ -4,14 +4,20 @@ import { STORAGE_KEY } from './config';
 
 export interface State {
   token: string;
+  user: any | null;
   setToken: (token: string) => void;
+  setUser: (user: any) => void;
+  logout: () => void;
 }
 
 export const useStore = create<State>()(
   persist(
     (set) => ({
       token: '',
-      setToken: (token: string) => set({ token })
+      user: null,
+      setToken: (token: string) => set({ token }),
+      setUser: (user: any) => set({ user }),
+      logout: () => set({ token: '', user: null })
     }),
     {
       name: STORAGE_KEY,
