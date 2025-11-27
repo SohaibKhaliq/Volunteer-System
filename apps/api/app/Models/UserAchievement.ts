036461ee9afd7ca34e32bd461d24c96949092381
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-27 16:26:45 +0500

    feat: create UserAchievement model for managing user achievements and relationships

---
+++ apps/api/app/Models/UserAchievement.ts
@@ -0,0 +1,33 @@
+import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
+import User from './User'
+import Achievement from './Achievement'
+
+export default class UserAchievement extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column({ columnName: 'user_id' })
+  public userId: number
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+  @column({ columnName: 'achievement_id' })
+  public achievementId: number
+
+  @belongsTo(() => Achievement)
+  public achievement: BelongsTo<typeof Achievement>
+
+  @column()
+  public metadata?: any
+
+  @column.dateTime({ columnName: 'awarded_at' })
+  public awardedAt: DateTime
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
