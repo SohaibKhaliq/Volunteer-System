646c6183d54da4d0068750e9f87dd4ebebe22a74
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-30 14:01:44 +0500

    feat: update Resource model to include additional fields and relationships for resource management

--- apps/api/app/Models/Resource.ts
+++ apps/api/app/Models/Resource.ts
@@ -1,6 +1,7 @@
-import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, belongsTo, BelongsTo, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
 import { DateTime } from 'luxon'
 import Organization from './Organization'
+import ResourceAssignment from './ResourceAssignment'
 
 export default class Resource extends BaseModel {
   @column({ isPrimary: true })
@@ -10,16 +11,40 @@ export default class Resource extends BaseModel {
   public name: string
 
   @column()
-  public quantity: number
+  public category: string
+
+  @column()
+  public description?: string
+
+  @column()
+  public quantityTotal: number
+
+  @column()
+  public quantityAvailable: number
 
   @column()
   public status: string
+
+  @column()
+  public location?: string
+
+  @column()
+  public serialNumber?: string
+
+  @column.dateTime()
+  public maintenanceDue?: DateTime | null
+
+  @column()
+  public attributes?: any
 
   @column()
   public organizationId?: number
 
   @belongsTo(() => Organization)
   public organization: BelongsTo<typeof Organization>
+
+  @hasMany(() => ResourceAssignment)
+  public assignments: HasMany<typeof ResourceAssignment>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
