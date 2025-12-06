7840fbcb4ea2a83f7cc4a587fe82d5a5f3ea1ac9
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-12-06 12:59:53 +0500

    Remove unused children prop from RadioGroupItem component

--- apps/app/src/components/atoms/radio-group.tsx
+++ apps/app/src/components/atoms/radio-group.tsx
@@ -15,9 +15,9 @@ RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
->(({ className, children, ...props }, ref) => {
+(({ className, ...props }, ref) => {
   return (
-    <RadioGroupPrimitive.Item
+        <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
         'aspect-square  h-4 w-4 rounded-full  border-2 border-gray-400 text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
