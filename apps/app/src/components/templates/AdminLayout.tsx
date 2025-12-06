c0acfc9f7fb56bdffb577a79db133979c744bec6
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-12-06 11:57:15 +0500

    Remove unused default React import in AdminLayout component

--- apps/app/src/components/templates/AdminLayout.tsx
+++ apps/app/src/components/templates/AdminLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Outlet, Link, useLocation } from 'react-router-dom';
 import { useApp } from '@/providers/app-provider';
 import { cn } from '@/lib/utils';
