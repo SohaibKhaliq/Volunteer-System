06bf186615a906cf306cd9c0c523f085590c2c61
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-12-23 20:30:04 +0500

    removed maps depedency

--- apps/app/src/components/molecules/address-input.tsx
+++ apps/app/src/components/molecules/address-input.tsx
@@ -1,13 +1,8 @@
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/atoms/popover';
-import { cn } from '@/lib/utils';
 import { BaseInputProps } from '@/types/form';
-import { FixType } from '@/types/utils';
-import { CaretSortIcon } from '@radix-ui/react-icons';
 import { forwardRef, useEffect, useState } from 'react';
-import usePlacesService from 'react-google-autocomplete/lib/usePlacesAutocompleteService';
-import { Button } from '../atoms/button';
-import { Command, CommandGroup, CommandInput, CommandItem } from '../atoms/command';
 import { useTranslation } from 'react-i18next';
+import { Input } from '../atoms/input';
+import { cn } from '@/lib/utils';
 
 interface TextProps extends BaseInputProps {
   value?: Location;
@@ -20,40 +15,30 @@ export interface Location {
   lng: number;
 }
 
-const AddressInput = forwardRef<HTMLDivElement, TextProps>((props, ref) => {
+const AddressInput = forwardRef<HTMLInputElement, TextProps>((props, ref) => {
   const { t } = useTranslation();
-
   const { label, optional = false, placeholder } = props;
-  const [open, setOpen] = useState(false);
-  const [location, setLocation] = useState<
-    Location & {
-      place_id: string | undefined;
-    }
-  >({
-    address: props.value?.address || placeholder || t('Enter an address...'),
-    lat: props.value?.lat || 0,
-    lng: props.value?.lng || 0,
-    place_id: undefined
-  });
-
-  const { placesService, placePredictions, getPlacePredictions } = usePlacesService({
-    apiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-    debounce: 500
-  });
+  
+  const [address, setAddress] = useState(props.value?.address || '');
 
   useEffect(() => {
-    if (props.value) {
-      setLocation({
-        address: props.value.address || placeholder || t('Enter an address...'),
-        lat: props.value.lat,
-        lng: props.value.lng,
-        place_id: undefined
-      });
+    if (props.value?.address) {
+      setAddress(props.value.address);
     }
   }, [props.value]);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newAddress = e.target.value;
+    setAddress(newAddress);
+    props.onChange({
+      address: newAddress,
+      lat: props.value?.lat || 0,
+      lng: props.value?.lng || 0
+    });
+  };
+
   return (
-    <div className={cn('flex flex-col gap-y-2.5')} ref={ref}>
+    <div className={cn('flex flex-col gap-y-2.5')}>
       <div className="font-medium flex flex-col">
         {label && (
           <label>
@@ -64,66 +49,17 @@ const AddressInput = forwardRef<HTMLDivElement, TextProps>((props, ref) => {
         {props.helperText && <span className="text-xs text-gray-500">{props.helperText}</span>}
       </div>
 
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            role="combobox"
-            aria-label={placeholder ?? t('Enter an address...')}
-            aria-expanded={open}
-            className="flex-1 justify-between w-full"
-          >
-            {location ? location.address : (placeholder ?? t('Enter an address...'))}
-            <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-screen">
-          <Command>
-            <CommandInput
-              placeholder={t('Start typing your address...')}
-              onValueChange={(value) => getPlacePredictions({ input: value })}
-            />
-
-            {
-              <CommandGroup>
-                {placePredictions.map((place) => (
-                  <CommandItem
-                    key={place.place_id}
-                    onSelect={() => {
-                      placesService?.getDetails(
-                        {
-                          placeId: place.place_id
-                        },
-                        (placeDetails: FixType) => {
-                          setLocation({
-                            address: placeDetails.formatted_address,
-                            lat: placeDetails.geometry.location.lat(),
-                            lng: placeDetails.geometry.location.lng(),
-                            place_id: place.place_id
-                          });
-
-                          props.onChange({
-                            address: placeDetails.formatted_address,
-                            lat: placeDetails.geometry.location.lat(),
-                            lng: placeDetails.geometry.location.lng()
-                          });
-                        }
-                      );
-                      setOpen(false);
-                    }}
-                  >
-                    {place.description}
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            }
-          </Command>
-        </PopoverContent>
-      </Popover>
+      <Input
+        ref={ref}
+        value={address}
+        onChange={handleChange}
+        placeholder={placeholder || t('Enter an address...')}
+        className="w-full"
+      />
     </div>
   );
 });
 
-AddressInput.displayName = 'TextInput';
+AddressInput.displayName = 'AddressInput';
 
 export default AddressInput;
