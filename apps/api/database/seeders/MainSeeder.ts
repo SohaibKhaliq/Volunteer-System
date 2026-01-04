27ccbb28875f3c0cea3356768a2ce08d4c250bf9
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2026-01-04 17:43:56 +0500

    feat: Add comprehensive seeders for various modules and update functional tests to include first/last names.

--- apps/api/database/seeders/MainSeeder.ts
+++ apps/api/database/seeders/MainSeeder.ts
@@ -9,89 +9,90 @@ export default class MainSeeder extends BaseSeeder {
     console.log('Starting database seeding...')
     
     // Core data
-    await this.seed(await import('./UserSeeder'))
-    await this.seed(await import('./OrganizationSeeder'))
+    await this.seed(await import('./modules/UserSeeder'))
+    await this.seed(await import('./modules/OrganizationSeeder'))
     
     // RBAC
-    await this.seed(await import('./RoleSeeder'))
-    await this.seed(await import('./PermissionSeeder'))
-    await this.seed(await import('./RolePermissionSeeder'))
-    await this.seed(await import('./UserRoleSeeder'))
+    await this.seed(await import('./modules/RoleSeeder'))
+    await this.seed(await import('./modules/PermissionSeeder'))
+    await this.seed(await import('./modules/RolePermissionSeeder'))
+    await this.seed(await import('./modules/UserRoleSeeder'))
     
     // Organization structure
-    await this.seed(await import('./TeamSeeder'))
-    await this.seed(await import('./OrganizationVolunteerSeeder'))
-    await this.seed(await import('./OrganizationInviteSeeder'))
+    await this.seed(await import('./modules/TeamSeeder'))
+    await this.seed(await import('./modules/OrganizationVolunteerSeeder'))
+    await this.seed(await import('./modules/OrganizationInviteSeeder'))
     
     // Events and opportunities
-    await this.seed(await import('./OpportunitySeeder'))
-    await this.seed(await import('./EventSeeder'))
-    await this.seed(await import('./ApplicationSeeder'))
+    await this.seed(await import('./modules/OpportunitySeeder'))
+    await this.seed(await import('./modules/EventSeeder'))
+    await this.seed(await import('./modules/ApplicationSeeder'))
     
     // Training
-    await this.seed(await import('./CourseSeeder'))
-    await this.seed(await import('./CourseEnrollmentSeeder'))
+    await this.seed(await import('./modules/CourseSeeder'))
+    await this.seed(await import('./modules/CourseEnrollmentSeeder'))
     
     // Tasks and assignments
-    await this.seed(await import('./TaskSeeder'))
-    await this.seed(await import('./AssignmentSeeder'))
+    await this.seed(await import('./modules/TaskSeeder'))
+    await this.seed(await import('./modules/AssignmentSeeder'))
     
     // Resources
-    await this.seed(await import('./ResourceSeeder'))
-    await this.seed(await import('./ResourceAssignmentSeeder'))
+    await this.seed(await import('./modules/ResourceSeeder'))
+    await this.seed(await import('./modules/ResourceAssignmentSeeder'))
     
     // Shifts
-    await this.seed(await import('./ShiftSeeder'))
-    await this.seed(await import('./ShiftTaskSeeder'))
-    await this.seed(await import('./ShiftAssignmentSeeder'))
+    await this.seed(await import('./modules/ShiftSeeder'))
+    await this.seed(await import('./modules/ShiftTaskSeeder'))
+    await this.seed(await import('./modules/ShiftAssignmentSeeder'))
     
     // Tracking
-    await this.seed(await import('./VolunteerHourSeeder'))
-    await this.seed(await import('./AttendanceSeeder'))
+    await this.seed(await import('./modules/VolunteerHourSeeder'))
+    await this.seed(await import('./modules/AttendanceSeeder'))
     
     // Feedback and surveys
-    await this.seed(await import('./SurveySeeder'))
-    await this.seed(await import('./SurveyResponseSeeder'))
+    await this.seed(await import('./modules/SurveySeeder'))
+    await this.seed(await import('./modules/SurveyResponseSeeder'))
     
     // Compliance
-    await this.seed(await import('./BackgroundCheckSeeder'))
-    await this.seed(await import('./ComplianceDocumentSeeder'))
-    await this.seed(await import('./ComplianceRequirementSeeder'))
+    await this.seed(await import('./modules/BackgroundCheckSeeder'))
+    await this.seed(await import('./modules/ComplianceDocumentSeeder'))
+    await this.seed(await import('./modules/ComplianceRequirementSeeder'))
     
     // Gamification
-    await this.seed(await import('./AchievementSeeder'))
-    await this.seed(await import('./UserAchievementSeeder'))
-    await this.seed(await import('./GamificationBadgeSeeder'))
-    await this.seed(await import('./UserBadgeSeeder'))
+    await this.seed(await import('./modules/AchievementSeeder'))
+    await this.seed(await import('./modules/UserAchievementSeeder'))
+    await this.seed(await import('./modules/GamificationBadgeSeeder'))
+    await this.seed(await import('./modules/UserBadgeSeeder'))
     
     // Documents
-    await this.seed(await import('./DocumentSeeder'))
-    await this.seed(await import('./DocumentAcknowledgmentSeeder'))
+    await this.seed(await import('./modules/DocumentSeeder'))
+    await this.seed(await import('./modules/DocumentAcknowledgmentSeeder'))
     
     // Communications
-    await this.seed(await import('./CommunicationSeeder'))
-    await this.seed(await import('./CommunicationLogSeeder'))
-    await this.seed(await import('./EngagementCampaignSeeder'))
+    await this.seed(await import('./modules/CommunicationSeeder'))
+    await this.seed(await import('./modules/CommunicationLogSeeder'))
+    await this.seed(await import('./modules/EngagementCampaignSeeder'))
     
     // Team membership
-    await this.seed(await import('./OrganizationTeamMemberSeeder'))
+    await this.seed(await import('./modules/OrganizationTeamMemberSeeder'))
     
     // Audit and logs
-    await this.seed(await import('./AuditLogSeeder'))
+    await this.seed(await import('./modules/AuditLogSeeder'))
     
     // Legacy features
-    await this.seed(await import('./TypeSeeder'))
-    await this.seed(await import('./HelpRequestSeeder'))
-    await this.seed(await import('./OfferSeeder'))
-    await this.seed(await import('./CarpoolingAdSeeder'))
+    await this.seed(await import('./modules/TypeSeeder'))
+    await this.seed(await import('./modules/HelpRequestSeeder'))
+    await this.seed(await import('./modules/OfferSeeder'))
+    await this.seed(await import('./modules/CarpoolingAdSeeder'))
     
     // System
-    await this.seed(await import('./ContactSubmissionSeeder'))
-    await this.seed(await import('./SystemSettingSeeder'))
-    await this.seed(await import('./ApiTokenSeeder'))
+    await this.seed(await import('./modules/ContactSubmissionSeeder'))
+    await this.seed(await import('./modules/SystemSettingSeeder'))
+    await this.seed(await import('./modules/FeatureFlagSeeder'))
+    await this.seed(await import('./modules/ApiTokenSeeder'))
     
     // Notifications
-    await this.seed(await import('./NotificationSeeder'))
+    await this.seed(await import('./modules/NotificationSeeder'))
 
     console.log('Database seeding completed successfully!')
   }
