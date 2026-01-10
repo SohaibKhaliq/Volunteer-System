cda03216bd327f0085348e0a765c006df27f1d51
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2026-01-10 16:12:11 +0500

    refactor: Add future configuration to createBrowserRouter for transition support

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -4,7 +4,11 @@ import routes from './lib/routes';
 import Providers from './providers';
 import OfflineBanner from './components/molecules/offline-banner';
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_startTransition: true
+  }
+});
 
 const App = () => {
   return (
