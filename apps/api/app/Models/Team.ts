c62793ff1351c7e6a47b732830ad4a80c6ee8cdb
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2026-01-11 23:28:22 +0500

    feat: Add capacity and minRequirementsEnabled attributes to Team model with relationships to OrganizationTeamMember, TeamCertificationRequirement, and ChatRoom

--- apps/api/app/Models/Team.ts
+++ apps/api/app/Models/Team.ts
@@ -2,6 +2,9 @@ import { BaseModel, column, belongsTo, BelongsTo, hasMany, HasMany } from '@ioc:
 import { DateTime } from 'luxon'
 import Organization from './Organization'
 import User from './User'
+import TeamCertificationRequirement from './TeamCertificationRequirement'
+import ChatRoom from './ChatRoom'
+import OrganizationTeamMember from './OrganizationTeamMember'
 
 export default class Team extends BaseModel {
   @column({ isPrimary: true })
@@ -16,6 +19,12 @@ export default class Team extends BaseModel {
   @column()
   public description?: string
 
+  @column()
+  public capacity?: number
+
+  @column()
+  public minRequirementsEnabled: boolean
+
   @column({ columnName: 'lead_user_id' })
   public leadUserId?: number
 
@@ -26,6 +35,21 @@ export default class Team extends BaseModel {
     foreignKey: 'leadUserId'
   })
   public lead: BelongsTo<typeof User>
+
+  @hasMany(() => OrganizationTeamMember, {
+    foreignKey: 'teamId'
+  })
+  public members: HasMany<typeof OrganizationTeamMember>
+ 
+  @hasMany(() => TeamCertificationRequirement, {
+    foreignKey: 'teamId'
+  })
+  public requirements: HasMany<typeof TeamCertificationRequirement>
+
+  @hasMany(() => ChatRoom, {
+    foreignKey: 'teamId'
+  })
+  public chatRooms: HasMany<typeof ChatRoom>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
