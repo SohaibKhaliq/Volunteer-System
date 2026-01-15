879e76eee2d35ef8fcec72a8d97e1266885ab9f9
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2026-01-15 14:16:18 +0500

    refactor: Simplify router creation by removing future transition options

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -4,17 +4,16 @@ import routes from './lib/routes';
 import Providers from './providers';
 import OfflineBanner from './components/molecules/offline-banner';
 
-const router = createBrowserRouter(routes, {
-  future: {
-    v7_startTransition: true
-  }
-});
+const router = createBrowserRouter(routes);
 
 const App = () => {
   return (
     <Providers>
       <OfflineBanner />
-      <RouterProvider router={router} fallbackElement={<LoadingSpinner />} />
+      <RouterProvider
+        router={router}
+        fallbackElement={<LoadingSpinner />}
+      />
     </Providers>
   );
 };
